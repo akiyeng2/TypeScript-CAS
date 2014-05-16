6614f793fb88e8a169470527efce463da1511e4e
scrblnrd3 <amalan.iyengar@gmail.com>
2014-05-16 19:07:11 -0400

    Added nice pretty intervals

--- src/convenience.js
+++ src/convenience.js
@@ -59,3 +59,36 @@ function unitTest(){
 function solve(first, second, guess) {
 	return new Equation(first).solve(new Equation(second), -10, 10, guess);
 }
+function getInfo(str, lower, upper, subintervals) {
+	var eqn = new Equation(str);
+	var criticals = eqn.criticals(lower, upper, (subintervals || 100));
+	var increasing = [];
+	var decreasing = [];
+	for(var i = 0; i < criticals.length; i++) {
+		if(criticals[i].sign == 1) {
+			increasing.push(criticals[i]);
+		}else{
+			decreasing.push(criticals[i]);
+		}
+	}
+	document.body.innerHTML = "";
+	document.body.innerHTML+=("$$f(x)=" + toTex(eqn.tree) + "$$" + "<br>");
+	var incIntervals = "";
+	
+	for(var i = 0; i < increasing.length; i++) {
+
+		incIntervals+=("(" + Math.round(increasing[i].lower*1e5)/1e5 + "," + Math.round(increasing[i].upper*1e5)/1e5+")\\cup");
+	}
+
+	var decIntervals = "";
+	
+	for(var i = 0; i < decreasing.length; i++) {
+
+		decIntervals+=("(" + Math.round(decreasing[i].lower*1e5)/1e5 + "," + Math.round(decreasing[i].upper*1e5)/1e5+")\\cup");
+	}
+	
+	document.body.innerHTML += "$$\\text{Increasing:} " + incIntervals.slice(0, -4) + "$$";
+	document.body.innerHTML += "$$\\text{Decreasing:} " + decIntervals.slice(0, -4) + "$$";
+
+	MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+}
