fdc9044537b25bda37edd06b93e41942ff1ae937
scrblnrd3 <amalan.iyengar@gmail.com>
2014-05-16 21:27:53 -0400

    Fixed bug with no critical points

--- src/Equation.js
+++ src/Equation.js
@@ -1,9 +1,9 @@
- 
 function Equation(expression) {
 	this.tree = null;
-	if(typeof expression == "string") {
+	if (typeof expression == "string") {
 		this.tree = toTree(shunt(expression));
-	}else if(expression instanceof Binary || expression instanceof Unary || expression instanceof Operand) {
+	} else if (expression instanceof Binary || expression instanceof Unary
+			|| expression instanceof Operand) {
 		this.tree = expression;
 	}
 }
@@ -13,13 +13,11 @@ Equation.prototype.toString = function() {
 };
 
 Equation.prototype.display = function() {
-document.body.innerHTML = "";
+	document.body.innerHTML = "";
 
-document.body.innerHTML+=("$$" + toTex(this.tree) +"$$" + "<br>");
-MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+	document.body.innerHTML += ("$$" + toTex(this.tree) + "$$" + "<br>");
+	MathJax.Hub.Queue([ "Typeset", MathJax.Hub ]);
 };
-
-
 
 Equation.prototype.differentiate = function() {
 	return new Equation(this.tree.differentiate());
@@ -29,128 +27,154 @@ Equation.prototype.evaluate = function(variables) {
 	return this.tree.evaluate(variables);
 };
 
-
 Equation.prototype.zero = function(lower, upper, guess, tolerance) {
-	var accuracy = tolerance || 1e-14; 
+	var accuracy = tolerance || 1e-14;
 	var maxIterations = 100;
 	var solution = false;
 	var error = Infinity;
-	
+
 	var x0 = guess;
 	var x1 = guess;
 	var deriv = this.differentiate();
-	
-	for(var i = 0; i < maxIterations; i++) {
-		var y = this.evaluate({"x": x0});
-		var dy = deriv.evaluate({"x": x0});
-		
-		x1 = x0 - y/dy;
-		if(isNaN(x1)) {
+
+	for ( var i = 0; i < maxIterations; i++) {
+		var y = this.evaluate({
+			"x" : x0
+		});
+		var dy = deriv.evaluate({
+			"x" : x0
+		});
+
+		x1 = x0 - y / dy;
+		if (isNaN(x1)) {
 			return NaN;
 		}
-		error = Math.abs(x0-x1)/Math.abs(x1);
+		error = Math.abs(x0 - x1) / Math.abs(x1);
 
-		if(error < accuracy) {
+		if (error < accuracy) {
 			solution = true;
 			break;
 		}
-		
+
 		x0 = x1;
-		
 
-		
 	}
-	
 
-		return {
+	return {
 		"solution" : x1,
 		"tolerance" : solution,
-		"error": this.evaluate({"x": x1})
+		"error" : this.evaluate({
+			"x" : x1
+		})
 	};
 
-	
 };
 
 Equation.prototype.optimize = function(lower, upper, subintervals) {
 	var n = subintervals || 100;
 
-	var size = (upper - lower)/n;
+	var size = (upper - lower) / n;
 
 	var df = this.differentiate();
 	var yPrime = NaN;
 	var lastYPrime = NaN;
 	var sign = function(x) {
-	    return typeof x === 'number' ? x ? x < 0 ? -1 : 1 : x === x ? 0 : NaN : NaN;
+		return typeof x === 'number' ? x ? x < 0 ? -1 : 1 : x === x ? 0 : NaN
+				: NaN;
 	};
 	var spots = [];
-	
-	for(var x = lower; x < upper; x+=size) {
-		 yPrime = df.evaluate({"x": x});
-		 if(sign(yPrime) == -1 * sign(lastYPrime)) {
-			 spots.push(x);
-		 }
-		 
-		 lastYPrime = yPrime;
-		
+
+	for ( var x = lower; x < upper; x += size) {
+		yPrime = df.evaluate({
+			"x" : x
+		});
+		if (sign(yPrime) == -1 * sign(lastYPrime)) {
+			spots.push(x);
+		}
+
+		lastYPrime = yPrime;
+
 	}
 
 	spots = spots.map(function(x) {
-		var zero = df.zero(x-size, x+size , x);
+		var zero = df.zero(x - size, x + size, x);
 
-		if(zero.tolerance) {	
+		if (zero.tolerance) {
 			return zero.solution;
-		} else if(x < 1e-10 || zero.solution < 1e-14) {
+		} else if (x < 1e-10 || zero.solution < 1e-14) {
 			return 0;
 		}
 	});
-	if(Math.abs(df.evaluate({"x": upper})) < 1e-14) {
+	if (Math.abs(df.evaluate({
+		"x" : upper
+	})) < 1e-14) {
 		spots.push(upper);
 	}
-	
-	if(Math.abs(df.evaluate({"x": lower})) < 1e-14) {
+
+	if (Math.abs(df.evaluate({
+		"x" : lower
+	})) < 1e-14) {
 		spots.unshift(lower);
 	}
 	return spots.filter(function(elem, pos) {
-	    return spots.indexOf(elem) == pos;
+		return spots.indexOf(elem) == pos;
 	});
-	
 
 };
 
 Equation.prototype.solve = function(curve, lower, upper, guess, tolerance) {
 	var toSolve = new Equation(new Binary("-", this.tree, curve.tree));
 	return toSolve.zero(lower, upper, guess, tolerance || 1e-14);
-}; 
+};
 
 Equation.prototype.criticals = function(lower, upper, subintervals) {
 	var criticals = this.optimize(lower, upper, (subintervals || 100));
-	var criticalIntervals  = [];
+	var criticalIntervals = [];
 	var sign = function(x) {
-	    return typeof x === 'number' ? x ? x < 0 ? -1 : 1 : x === x ? 0 : NaN : NaN;
+		return typeof x === 'number' ? x ? x < 0 ? -1 : 1 : x === x ? 0 : NaN
+				: NaN;
 	};
-	if(Math.abs(criticals[0] - lower) > 1e-10) {
+
+	if (criticals.length == 0) {
+
+		return [{
+			"lower" : lower,
+			"upper" : upper,
+			"sign" : sign(this.differentiate().evaluate({
+				"x" : (lower + (lower + upper)/(subintervals || 100)) 
+			}))
+		}];
+	}
+	if (Math.abs(criticals[0] - lower) > 1e-10) {
 		criticals.unshift(lower);
 	}
-	
-	if(Math.abs(criticals[criticals.length-1] - upper) > 1e-10) {
+
+	if (Math.abs(criticals[criticals.length - 1] - upper) > 1e-10) {
 		criticals.push(upper);
 	}
+
 	var numIntervals = 0;
-	for(var i = 0; i < criticals.length - 1; i++) {
-		var sgn = sign(this.differentiate().evaluate({"x": (criticals[i]+criticals[i+1])/2}));
+	for ( var i = 0; i < criticals.length - 1; i++) {
+		var sgn = sign(this.differentiate().evaluate({
+			"x" : (criticals[i] + criticals[i + 1]) / 2
+		}));
 		var prevSign = 0;
-		if(numIntervals.length > 0) {
+		if (numIntervals.length > 0) {
 			prevSign = criticalIntervals[numIntervals - 1];
 		}
-		if(prevSign == sgn) {
-			criticalIntervals[numIntervals - 1].upper = criticals[i+1];
-		}else{
-			criticalIntervals.push({"lower": criticals[i], "upper": criticals[i+1], "sign": sgn});
+		if (prevSign == sgn) {
+			criticalIntervals[numIntervals - 1].upper = criticals[i + 1];
+		} else {
+			criticalIntervals.push({
+				"lower" : criticals[i],
+				"upper" : criticals[i + 1],
+				"sign" : sgn
+			});
 			numIntervals++;
 		}
-		
+
 	}
-	
+
 	return criticalIntervals;
-}; 
+};
 f = new Equation("sin(x)");
