482f354ad9d5ab7f7383bdac9b91c147f9876e8f
scrblnrd3 <amalan.iyengar@gmail.com>
2014-05-16 20:45:31 -0400

    Added concavity

--- src/convenience.js
+++ src/convenience.js
@@ -59,8 +59,8 @@ function unitTest(){
 function solve(first, second, guess) {
 	return new Equation(first).solve(new Equation(second), -10, 10, guess);
 }
-function getInfo(str, lower, upper, subintervals) {
-	var eqn = new Equation(str);
+function getInfo(eqn, lower, upper, subintervals) {
+	var eqn = (typeof eqn === "string"? new Equation(eqn) : eqn);
 	var criticals = eqn.criticals(lower, upper, (subintervals || 100));
 	var increasing = [];
 	var decreasing = [];
@@ -72,23 +72,50 @@ function getInfo(str, lower, upper, subintervals) {
 		}
 	}
 	document.body.innerHTML = "";
-	document.body.innerHTML+=("$$f(x)=" + toTex(eqn.tree) + "$$" + "<br>");
+	document.body.innerHTML+=("${\nf(x)=" + toTex(eqn.tree))+"}$<br><br>";
 	var incIntervals = "";
 	
 	for(var i = 0; i < increasing.length; i++) {
 
-		incIntervals+=("(" + Math.round(increasing[i].lower*1e5)/1e5 + "," + Math.round(increasing[i].upper*1e5)/1e5+")\\cup");
+		incIntervals+=("(" + Math.round(increasing[i].lower*1e5)/1e5 + ",\\nobreakspace" + Math.round(increasing[i].upper*1e5)/1e5+")\\cup");
 	}
 
 	var decIntervals = "";
 	
 	for(var i = 0; i < decreasing.length; i++) {
 
-		decIntervals+=("(" + Math.round(decreasing[i].lower*1e5)/1e5 + "," + Math.round(decreasing[i].upper*1e5)/1e5+")\\cup");
+		decIntervals+=("(" + Math.round(decreasing[i].lower*1e5)/1e5 + ",\\nobreakspace" + Math.round(decreasing[i].upper*1e5)/1e5+")\\cup");
 	}
 	
-	document.body.innerHTML += "$$\\text{Increasing:} " + incIntervals.slice(0, -4) + "$$";
-	document.body.innerHTML += "$$\\text{Decreasing:} " + decIntervals.slice(0, -4) + "$$";
+	document.body.innerHTML += "$\\text{Increasing: } " + incIntervals.slice(0, -4) + "\\\\";
+	document.body.innerHTML += "\\text{Decreasing: } " + decIntervals.slice(0, -4) + "$";
 
+	var inflections = eqn.differentiate().criticals(lower, upper, (subintervals || 100));
+	
+	var ccup = [];
+	var ccdown = [];
+	for(var i = 0; i < inflections.length; i++) {
+		if(inflections[i].sign == 1) {
+			ccup.push(inflections[i]);
+		}else{
+			ccdown.push(inflections[i]);
+		}
+	}
+	var ccUpIntervals = "";
+	
+	for(var i = 0; i < ccup.length; i++) {
+
+		ccUpIntervals+=("(" + Math.round(ccup[i].lower*1e5)/1e5 + ",\\nobreakspace" + Math.round(ccup[i].upper*1e5)/1e5+")\\cup");
+	}
+
+	var ccDownIntervals = "";
+	
+	for(var i = 0; i < ccdown.length; i++) {
+
+		ccDownIntervals+=("(" + Math.round(ccdown[i].lower*1e5)/1e5 + ",\\nobreakspace" + Math.round(ccdown[i].upper*1e5)/1e5+")\\cup");
+	}
+
+	document.body.innerHTML += "<br><br>$\\text{Concave Up: } " + ccUpIntervals.slice(0, -4) + "\\\\";
+	document.body.innerHTML += "\\text{Concave Down: } " + ccDownIntervals.slice(0, -4) + "$";
 	MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
 }
