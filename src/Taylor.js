14898f18174a8e8327d062bf17fcaff9924e7cbd
scrblnrd3 <amalan.iyengar@gmail.com>
2014-05-26 12:57:14 -0400

    Added simplification on multiply, add, and subtract

---
+++ src/Taylor.js
@@ -0,0 +1,30 @@
+Equation.prototype.Taylor = function(order, center, respect) {
+	var wrt = respect || "x";
+	var center = center || 0;
+	var lastDerivative = this;
+	var coefficients = [];
+	for(var i = 0; i < order; i++) {
+		coefficients.push(lastDerivative.evaluate(wrt, center));
+		lastDerivative = lastDerivative.differentiate(wrt);
+	}
+
+	return coefficients;
+}
+
+Equation.prototype.integrate = function(lower, upper, respect, subintervals) {
+	
+	var subintervals = subintervals || 100000;
+	var size = (upper - lower) / subintervals;
+	var wrt = respect || "x";
+
+	var sum = 0;
+
+	for(var i = lower; i < upper; i += size) {
+		var left = this.evaluate(wrt, i);
+		var right = this.evaluate(wrt, i + size);
+
+		sum += ((left + right) / 2) * size;
+	}
+
+	return sum;
+}
